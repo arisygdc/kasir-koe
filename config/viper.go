b3fb9b22e194ff96677c8e8105fe914da539da64
arisygdc <arisycso@gmail.com>
2021-12-02 22:52:46 +0700

    delete access token

--- config/viper.go
+++ config/viper.go
@@ -1,23 +1,20 @@
 package config
 
 import (
-	"time"
-
 	"github.com/spf13/viper"
 )
 
 type Config struct {
-	Db_driver           string        `mapstructure:"DB_DRIVER"`
-	Db_source           string        `mapstructure:"PG_SOURCE"`
-	Db_host             string        `mapstructure:"DB_HOST"`
-	Db_port             string        `mapstructure:"DB_PORT"`
-	Db_name             string        `mapstructure:"DB_NAME"`
-	Db_username         string        `mapstructure:"DB_USERNAME"`
-	Db_password         string        `mapstructure:"DB_PASSWORD"`
-	Db_sslmode          string        `mapstructure:"DB_SSLMODE"`
-	ServerAddress       string        `mapstructure:"SERVER_ADDRESS"`
-	Api_key             string        `mapstructure:"API_KEY"`
-	AccessTokenDuration time.Duration `mapstructure:"ACCESS_TOKEN_DURATION"`
+	Db_driver     string `mapstructure:"DB_DRIVER"`
+	Db_source     string `mapstructure:"PG_SOURCE"`
+	Db_host       string `mapstructure:"DB_HOST"`
+	Db_port       string `mapstructure:"DB_PORT"`
+	Db_name       string `mapstructure:"DB_NAME"`
+	Db_username   string `mapstructure:"DB_USERNAME"`
+	Db_password   string `mapstructure:"DB_PASSWORD"`
+	Db_sslmode    string `mapstructure:"DB_SSLMODE"`
+	ServerAddress string `mapstructure:"SERVER_ADDRESS"`
+	Api_key       string `mapstructure:"API_KEY"`
 }
 
 func LoadConfig(path string) (config Config, err error) {
